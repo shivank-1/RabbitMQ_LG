1e6b48052e9e54505bc13208670464f721a9beb2
shivank-1 <shivankdevliyal1@gmail.com>
2025-04-23 23:35:20 +0530

    Update

--- Producer_oops.py
+++ Producer_oops.py
@@ -86,7 +86,7 @@ class RabbitMq(metaclass=MetaClass):
 if __name__ == "__main__":
 
     ## Calling a 'class' with its parameters creates a new object (or instance) of that class.
-    # REMEMBER here we ae not calling the functions of above class but instead the class itself with the parameters of __init__ function
+    # REMEMBER here we ae nt calling the functions of above class but instead the class itself with the parameters of __init__ function
     # init paramters are called class parameter
 #V.Imp  ## Therefore 'object creation' of classes is taking place here## 
 
